505c16577fd6072607ea99a745dfce455bbbedfe
Vitalii-Ohol <ohol.vitaliy@gmail.com>
2020-08-23 19:57:32 +0300

    removed unused 'wget' dependency from setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ destination="$HOME/bin"
 [ -d "$destination" ] || mkdir "$destination"
 [ -f "$destination/termux-url-opener" ] && rm -f "$destination/termux-url-opener"
 
-wget -P "$destination/" "https://raw.githubusercontent.com/ohol-vitaliy/termux-url-opener/master/termux-url-opener"
+curl -o "$destination/termux-url-opener" "https://raw.githubusercontent.com/ohol-vitaliy/termux-url-opener/master/termux-url-opener"
 chmod +x "$destination/termux-url-opener"
 termux-fix-shebang "$destination/termux-url-opener"
 
